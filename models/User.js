389bed4de062b5bf3b33441cd67b689883ed19fd
anes255 <anesgaher3000@gmail.com>
2025-11-19 20:07:58 +0000

    Update User.js

--- models/User.js
+++ models/User.js
@@ -3,7 +3,8 @@ const mongoose = require('mongoose');
 const userSchema = new mongoose.Schema({
   email: { type: String, required: true, unique: true, lowercase: true },
   password: { type: String, required: true },
-  userType: { type: String, enum: ['client', 'freelancer'], required: true },
+  userType: { type: String, enum: ['client', 'freelancer', 'admin', 'maintenance'], required: true },
+  role: { type: String, enum: ['user', 'admin', 'maintenance'], default: 'user' },
   name: { type: String, required: true },
   profilePicture: { type: String, default: '' },
   isApproved: { type: Boolean, default: false },
