912fa2e97c2cd36779f84d1bd08c9afbf4824a24
anes255 <anesgaher3000@gmail.com>
2025-11-21 10:25:17 +0000

    Update order.js

--- routes/order.js
+++ routes/order.js
@@ -5,7 +5,12 @@ const { verifyToken } = require('../middleware/auth');
 
 router.post('/', verifyToken, orderController.createOrder);
 router.get('/my', verifyToken, orderController.getMyOrders);
+router.get('/all', verifyToken, orderController.getAllOrders);
+router.get('/payment-stats', verifyToken, orderController.getPaymentStats);
+router.get('/:id', verifyToken, orderController.getOrderById);
 router.put('/:id/status', verifyToken, orderController.updateOrderStatus);
 router.post('/:id/review', verifyToken, orderController.addReview);
+router.post('/:id/message', verifyToken, orderController.sendMessage);
+router.post('/:id/approve-payment', verifyToken, orderController.approvePayment);
 
 module.exports = router;
