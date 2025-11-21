c206ccd1ace366e7c36e36ee4cdf25832112ecbc
anes255 <anesgaher3000@gmail.com>
2025-11-21 10:25:06 +0000

    Update maintenance.js

--- routes/maintenance.js
+++ routes/maintenance.js
@@ -1,12 +1,13 @@
 const express = require('express');
 const router = express.Router();
 const maintenanceController = require('../controllers/maintenanceController');
-const { verifyToken, verifySuperAdmin } = require('../middleware/auth');
+const { verifyToken, isMaintenance } = require('../middleware/auth');
 
-router.get('/settings', verifyToken, verifySuperAdmin, maintenanceController.getSettings);
-router.put('/settings', verifyToken, verifySuperAdmin, maintenanceController.updateSettings);
-router.get('/errors', verifyToken, verifySuperAdmin, maintenanceController.getErrors);
-router.delete('/errors', verifyToken, verifySuperAdmin, maintenanceController.clearErrors);
-router.get('/stats', verifyToken, verifySuperAdmin, maintenanceController.getStats);
+router.get('/settings', verifyToken, isMaintenance, maintenanceController.getSettings);
+router.put('/settings', verifyToken, isMaintenance, maintenanceController.updateSettings);
+router.get('/errors', verifyToken, isMaintenance, maintenanceController.getErrors);
+router.delete('/errors', verifyToken, isMaintenance, maintenanceController.clearErrors);
+router.get('/stats', verifyToken, isMaintenance, maintenanceController.getStats);
+router.get('/payment-stats', verifyToken, isMaintenance, maintenanceController.getPaymentStats);
 
 module.exports = router;
